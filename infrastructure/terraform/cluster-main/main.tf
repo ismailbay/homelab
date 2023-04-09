4cde6f783ba59ad0e377b39fc5c7020fcc462605
Ismail Bay <ismailbay@gmail.com>
2023-04-09 17:22:04 +0200

    fix: ignore changes on qemu_os

--- infrastructure/terraform/cluster-main/main.tf
+++ infrastructure/terraform/cluster-main/main.tf
@@ -44,7 +44,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_master" {
 
   lifecycle {
     ignore_changes = [
-      network, target_node
+      network, target_node, qemu_os
     ]
   }
 }
@@ -95,7 +95,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_worker" {
 
   lifecycle {
     ignore_changes = [
-      network, target_node
+      network, target_node, qemu_os
     ]
   }
 }
